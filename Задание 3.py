6a5af3aa6d8140414e759c024ea36e0e3bb3c39a
Jack <martellianno12345@gmail.com>
2024-06-02 19:18:44 +0300

    Задание 3.py

---
+++ Задание 3.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    while True:
+        ret, frame = cap.read()
+
+        # Смена цветого пространства с RGB на HSV для более гибкой работы с изображением
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Диапазон красного цвета в HSV
+        red1 = np.array([0, 50, 50])
+        red2 = np.array([10, 255, 255])
+
+        mask = cv2.inRange(hsv, red1, red2)  # создаём маску
+
+        # Поиск контуров красного объекта
+        contr, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Конутры метки
+        if len(contr) > 0:
+            c = max(contr, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            center = (int(x), int(y))
+            cv2.circle(frame, center, int(radius), (0, 255, 0), 2)
+            if x > frame.shape[1] / 2:  # проверяем находиться ли метка по координате x в правой части экрана
+                cv2.putText(frame, 'POPAL', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)  # пишем текст
+
+        cv2.imshow('ISHY KRASNUI METKY', frame)
+
+        if cv2.waitKey(1):
+            break
